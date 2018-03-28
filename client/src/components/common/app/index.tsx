a730b96aec649a5a173bf191f1e07498cb89c978
Vadim Chernikov <vadim.dom_96@mail.ru>
2018-03-28 12:34:30 +0300

    fix2

--- client/src/components/common/app/index.tsx
+++ client/src/components/common/app/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
-interface IProps {
-  children: React.ReactNode;
-}
+// interface IProps {
+//   children: React.ReactNode;
+// }
 
-const App = (props: IProps) => {
+// const App = (props: IProps) => {
+const App = () => {
   return (
     <div>
     </div>
