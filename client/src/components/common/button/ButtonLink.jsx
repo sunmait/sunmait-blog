c58d2a60f89a81cd4408c746813f2fe1d5322075
hannahbugai <hannah97bugai@gmail.com>
2018-07-05 11:48:39 +0300

    add bem, variables, common button component, redesign home page

---
+++ client/src/components/common/button/ButtonLink.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import 'assets/styles/Button.less';
+import { Link } from 'react-router-dom';
+
+const ButtonLink = props => {
+  const { buttonColor, linkUrl, label } = props;
+
+  return (
+    <Link
+      className={`custom-button ${buttonColor}`}
+      to={linkUrl}
+    >
+      {label}
+    </Link>
+  );
+};
+
+ButtonLink.defaultProps = {
+  label: 'Button',
+  buttonColor: 'default',
+};
+
+export default ButtonLink;
+
+
+
