585971faa1a55d38e7da2fc832d8a161aa75a5d5
Verigo Aleksander <Verigiens@yandex.ru>
2019-07-12 15:58:56 +0300

    Window Prewiev

--- client/src/components/pages/add-post-page/add-post-form/AddPostFormContainer.js
+++ client/src/components/pages/add-post-page/add-post-form/AddPostFormContainer.js
@@ -1,9 +1,15 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { formValueSelector } from 'redux-form';
+
 import { setTextareaSelectionValues } from 'redux/modules/posts/postsActions';
 import EditPost from './AddPostForm';
 
+const userFormSelector = formValueSelector('post');
+
 const mapStateToProps = (state, props) => {
+  const postData = userFormSelector(state, 'Description');
+
   const initialValues =
     state.post.post && state.post.post.id === props.postId
       ? {
@@ -18,8 +24,10 @@ const mapStateToProps = (state, props) => {
           textareaSelectionStart: 0,
           textareaSelectionEnd: 0,
         };
+
   return {
     initialValues,
+    postData,
   };
 };
 
