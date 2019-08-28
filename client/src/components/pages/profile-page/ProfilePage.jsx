f2b79e2f44cefe45ac7068cb346fef4603130fea
Nikita Pilinka <mineralsfree@gmail.com>
2019-08-28 16:01:57 +0300

    ProfilePage component rewritten

--- client/src/components/pages/profile-page/ProfilePage.jsx
+++ client/src/components/pages/profile-page/ProfilePage.jsx
@@ -1,59 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-import Avatar from '@material-ui/core/Avatar';
-
-import { getBEMClasses } from 'helpers//BEMHelper';
-import UserInfoForm from './user-info-form/index.jsx';
-import ConfirmationModal from 'components/common/confirmation-modal/ConfirmationModal.jsx';
-import { UserPostListContainer } from '../../containers/postsList';
-import NavMenu from 'components/common/navMenu';
-import Loader from 'components/common/loader';
-
+import React, { useCallback, useEffect, useState } from 'react';
 import '../../../assets/styles/ProfilePage.css';
 
-const userProfile = 'user-profile';
-const bemClasses = getBEMClasses([userProfile]);
+import { getBEMClasses } from '../../../helpers/BEMHelper';
+import { Route } from 'react-router-dom';
+import { navTabs } from './navTabs';
+import { Helmet } from 'react-helmet';
+import { Avatar } from '@material-ui/core';
+import UserInfoForm from './user-info-form/index.jsx';
+import ConfirmationModal from '../../common/confirmation-modal/ConfirmationModal';
+import Loader from '../../common/loader';
+import NavMenu from '../../common/navMenu';
+import { UserPostListContainer } from '../../containers/postsList';
+const userProfile = 'userProfile';
+const profilePageCn = getBEMClasses([userProfile]);
 
-class ProfilePage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      openDialog: false,
-    };
-  }
-
-  componentDidMount() {
-    const { setUserId, match } = this.props;
-    setUserId(match.params.userId);
-    this.reloadProfile(this.props);
-  }
-
-  componentWillUnmount() {
-    this.props.setCurrentUserPostsFetchingStatus(true);
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (newProps.location.pathname !== this.props.location.pathname) {
-      this.reloadProfile(newProps);
-    }
-  }
-
-  reloadProfile = props => {
-    props.getUser(props.match.params.userId);
-    props.getCurrentUserPosts(props.match.params.userId);
+export const ProfilePage = ({
+  getUser,
+  updateUser,
+  getCurrentUserPosts,
+  setCurrentUserPostsFetchingStatus,
+  setUserId,
+  profile,
+  user,
+  userFormValues,
+  currentUserPostsFetchingStatus,
+  match,
+}) => {
+  const reloadProfile = userId => {
+    getUser(userId);
+    getCurrentUserPosts(userId);
   };
-
-  handleSubmitChanges = () => {
-    this.setState({
-      openDialog: true,
-    });
+  const [openDialog, setOpenDialog] = useState(false);
+  useEffect(
+    () => {
+      setUserId(match.params.userId);
+      reloadProfile(match.params.userId);
+      return () => setCurrentUserPostsFetchingStatus(true);
+    },
+    [match.params.userId]
+  );
+  const handleSubmitChanges = () => {
+    setOpenDialog(true);
   };
-
-  change = () => {
-    const { userFormValues, user } = this.props;
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+  const change = () => {
     const updatedUserData = {
       id: user.id,
       changedUser: {
@@ -61,62 +53,46 @@ class ProfilePage extends React.Component {
         secondName: userFormValues.LastName,
       },
     };
-
-    this.props.updateUser(updatedUserData);
-    this.handleClose();
+    updateUser(updatedUserData);
+    handleClose();
   };
-
-  handleClose = event => {
-    this.setState({
-      openDialog: false,
-    });
-  };
-
-  renderProfileHeader() {
-    const { profile } = this.props;
-
+  const renderProfileHeader = () => {
     if (profile) {
       const fullName = `${profile.FirstName} ${profile.LastName}`;
-
       return (
-        <div className={bemClasses('header')} data-cy="header">
-          <Helmet title={fullName} />
+        <div className={profilePageCn('header')} data-cy="headers">
+          <Helmet />
           <Avatar
-            alt="Username"
-            src={this.props.profile.PhotoUrl}
-            className={bemClasses('avatar')}
+            alt="UserAvatar"
+            src={profile.PhotoUrl}
+            className={profilePageCn('avatar')}
             data-cy="header__avatar"
           />
-          <div data-cy={bemClasses('header-name-surname')}>{fullName}</div>
+          <div data-cy={profilePageCn('header-name-surname')}>{fullName}</div>
         </div>
       );
     }
-
-    return null;
-  }
-
-  renderProfileForm = () => {
-    const { profile, user } = this.props;
-
+  };
+  const renderProfileForm = (profile, user, handleClose, change, handleSubmitChanges, openDialog) => {
     if (profile && user && user.id === profile.id) {
       return (
-        <React.Fragment>
-          <UserInfoForm onSubmit={this.handleSubmitChanges} />
+        <>
+          <UserInfoForm onSubmit={handleSubmitChanges} />
           <ConfirmationModal
-            isOpen={this.state.openDialog}
-            handleClose={this.handleClose}
-            handleSubmit={this.change}
+            isOpen={openDialog}
+            handleClose={handleClose}
+            change
+            handleSubmit={change}
             modalTitle="Confirm changes"
           />
-        </React.Fragment>
+        </>
       );
     }
     return <div>This is an info page</div>;
   };
 
-  render = () => {
-    const { match } = this.props;
-    const navTabs = [
+  const navTabs = useCallback(
+    match => [
       {
         text: 'Info',
         url: match.url,
@@ -125,32 +101,27 @@ class ProfilePage extends React.Component {
         text: 'Posts',
         url: `${match.url}/posts`,
       },
-    ];
-
-    return (
-      <div className="content-wrapper content-wrapper--with-grey-background">
-        <div className="content">
-          {this.props.currentUserPostsFetchingStatus ? (
-            <Loader />
-          ) : (
-            <React.Fragment>
-              {this.renderProfileHeader()}
-              <NavMenu tabs={navTabs} />
-              <Route exact path={match.url} render={() => this.renderProfileForm()} />
-              <Route path={`${match.url}/posts`} render={UserPostListContainer} />
-            </React.Fragment>
-          )}
-        </div>
+    ],
+    [match]
+  );
+  return (
+    <div className="content-wrapper content-wrapper--with-grey-background">
+      <div className="content">
+        {currentUserPostsFetchingStatus ? (
+          <Loader />
+        ) : (
+          <>
+            {renderProfileHeader()}
+            <NavMenu tabs={navTabs(match)} />
+            <Route
+              exact
+              path={match.url}
+              render={() => renderProfileForm(profile, user, handleClose, change, handleSubmitChanges, openDialog)}
+            />
+            <Route path={`${match.url}/posts`} render={UserPostListContainer} />
+          </>
+        )}
       </div>
-    );
-  };
-}
-
-ProfilePage.propTypes = {
-  updateUser: PropTypes.func.isRequired,
-  getUser: PropTypes.func.isRequired,
-  setCurrentUserPostsFetchingStatus: PropTypes.func.isRequired,
-  setUserId: PropTypes.func.isRequired,
+    </div>
+  );
 };
-
-export default ProfilePage;
